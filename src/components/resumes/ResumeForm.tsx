afc07d239895aa1ff6e8c6d3f2749389a7b5558b
TrueVinci <kattamuri1506@gmail.com>
2025-03-03 01:10:18 -0600

    Update ResumeForm.tsx resume form error fix

--- src/components/resumes/ResumeForm.tsx
+++ src/components/resumes/ResumeForm.tsx
@@ -1,1 +1,11 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please <a class=\"__boltUpgradePlan__\">Upgrade</a> to keep chatting, or you can continue coding for free in the editor.","providerLimitHit":false,"isRetryable":true}
+import React from 'react';
+
+const ResumeForm: React.FC = () => {
+  return (
+    <div>
+      {/* Resume form content will go here */}
+    </div>
+  );
+};
+
+export default ResumeForm;
