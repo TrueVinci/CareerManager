84f0c2c11542d185a52eac5abaa78a03adb7cbcd
TrueVinci <kattamuri1506@gmail.com>
2025-03-03 22:55:26 -0600

    Update App.tsx resume viewer fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,48 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './components/Dashboard';
-import JobList from './components/jobs/JobList';
-import JobDetail from './components/jobs/JobDetail';
-import JobForm from './components/jobs/JobForm';
-import ResumeList from './components/resumes/ResumeList';
-import ResumeDetail from './components/resumes/ResumeDetail';
-import ResumeForm from './components/resumes/ResumeForm';
-import ApplicationList from './components/applications/ApplicationList';
-import ApplicationDetail from './components/applications/ApplicationDetail';
-import ApplicationForm from './components/applications/ApplicationForm';
 import CompanyList from './components/companies/CompanyList';
 import CompanyDetail from './components/companies/CompanyDetail';
 import CompanyForm from './components/companies/CompanyForm';
+import JobList from './components/jobs/JobList';
+import JobDetail from './components/jobs/JobDetail';
+import JobForm from './components/jobs/JobForm';
+import ApplicationList from './components/applications/ApplicationList';
+import ApplicationDetail from './components/applications/ApplicationDetail';
+import ApplicationForm from './components/applications/ApplicationForm';
+import ResumeList from './components/resumes/ResumeList';
+import ResumeDetail from './components/resumes/ResumeDetail';
+import ResumeForm from './components/resumes/ResumeForm';
+import ResumeViewer from './components/resumes/ResumeViewer';
 
-function App() {
+const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
-          
-          {/* Jobs Routes */}
-          <Route path="jobs" element={<JobList />} />
-          <Route path="jobs/:id" element={<JobDetail />} />
-          <Route path="jobs/new" element={<JobForm />} />
-          <Route path="jobs/:id/edit" element={<JobForm />} />
-          
-          {/* Resumes Routes */}
-          <Route path="resumes" element={<ResumeList />} />
-          <Route path="resumes/:id" element={<ResumeDetail />} />
-          <Route path="resumes/new" element={<ResumeForm />} />
-          <Route path="resumes/:id/edit" element={<ResumeForm />} />
-          
-          {/* Applications Routes */}
-          <Route path="applications" element={<ApplicationList />} />
-          <Route path="applications/:id" element={<ApplicationDetail />} />
-          <Route path="applications/new" element={<ApplicationForm />} />
-          <Route path="applications/:id/edit" element={<ApplicationForm />} />
-          
-          {/* Companies Routes */}
           <Route path="companies" element={<CompanyList />} />
           <Route path="companies/:id" element={<CompanyDetail />} />
           <Route path="companies/new" element={<CompanyForm />} />
           <Route path="companies/:id/edit" element={<CompanyForm />} />
+          <Route path="jobs" element={<JobList />} />
+          <Route path="jobs/:id" element={<JobDetail />} />
+          <Route path="jobs/new" element={<JobForm />} />
+          <Route path="jobs/:id/edit" element={<JobForm />} />
+          <Route path="applications" element={<ApplicationList />} />
+          <Route path="applications/:id" element={<ApplicationDetail />} />
+          <Route path="applications/new" element={<ApplicationForm />} />
+          <Route path="applications/:id/edit" element={<ApplicationForm />} />
+          <Route path="resumes" element={<ResumeList />} />
+          <Route path="resumes/:id" element={<ResumeDetail />} />
+          <Route path="resumes/new" element={<ResumeForm />} />
+          {/* New route for the resume viewer */}
+          <Route path="resumes/view/:id" element={<ResumeViewer />} />
         </Route>
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
